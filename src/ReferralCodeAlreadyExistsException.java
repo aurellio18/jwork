ec9a742a51689e72ee0a73eb832427acd2032b33
aurellio18 <aurellio.fishandy@ui.ac.id>
2021-05-06 17:27:32 +0700

    Modul 7 - CS _ Aurellio Fishandy

---
+++ src/ReferralCodeAlreadyExistsException.java
@@ -0,0 +1,15 @@
+public class ReferralCodeAlreadyExistsException extends Exception{
+    private Bonus referral_error;
+
+    public ReferralCodeAlreadyExistsException(Bonus referral_input){
+        super("Refferral Code: ");
+        referral_error = referral_input;
+
+    }
+
+    @Override
+    public String getMessage(){
+        return super.getMessage()+ referral_error.getReferralCode() + "already exists.";
+    }
+}
+
